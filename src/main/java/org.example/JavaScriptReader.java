cfaccc9d917656ce2883b9d59acafc07fbee1e25
Alex Fomin <djartvi@yandex.ru>
2023-05-01 10:57:51 +0400

    add js reader

---
+++ src/main/java/org.example/JavaScriptReader.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import javax.script.ScriptException;
+import java.io.*;
+
+public class JavaScriptReader {
+
+    String execFile = "/Users/alex/Desktop/any_replacer/src/main/resources/xml_translator/main.js";
+
+    public void runScript() throws IOException, InterruptedException {
+        // Создаем объект ProcessBuilder для запуска команды
+        ProcessBuilder processBuilder = new ProcessBuilder("node", "main.js");
+
+        // Устанавливаем рабочую директорию (необязательно)
+        processBuilder.directory(new File("/Users/alex/Desktop/any_replacer/src/main/resources/xml_translator/"));
+
+        // Запускаем команду
+        Process process = processBuilder.start();
+
+        // Получаем результаты выполнения команды
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        // Считываем и выводим результат
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        // Ожидаем завершения процесса
+        process.waitFor();
+
+        // Выводим код завершения процесса
+        System.out.println("Exit code: " + process.exitValue());
+    }
+}
